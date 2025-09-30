15403eb27e0bb1e366333391677fd7ffc236eaaf
siyabonga <siyabongakhanyile76@gmail.com>
2025-09-30 16:57:49 +0200

    mod:implemented despatch of redux

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,14 @@
 import { useAppSelector, useAppDispatch } from "../../reduxHooks";
 import { login } from "../features/authenticate";
 
-
-
 export default function Login() {
   const shoppingList = useAppSelector((state) => state.shoppingList);
   const dispatch = useAppDispatch();
 
   return (
     <div>
-      <h2 >{shoppingList.Name}</h2>
-      <button onClick={() => dispatch(login("Masiya"))}>
-  display
-</button>
-
+      <h2>{shoppingList.Name}</h2>
+      <button onClick={() => dispatch(login("Masiya "))}>display</button>
     </div>
   );
 }
