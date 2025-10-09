e8e7a9a0364a6dd6d05b8df9ba626f01e60194be
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-09 09:54:53 +0200

    mod:put back strict mode in main,the project is going to production so dev mode error do not matter now

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "../store.ts";
-import "sonner/dist/styles.css";
+import {store} from "../store.ts";
+import 'sonner/dist/styles.css';
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
