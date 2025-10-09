fe180d7416949f1c9f3c1bece98d478d6e3b0541
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-09 09:51:51 +0200

    feature:added sharing feature

--- store.ts
+++ store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userManagementReducer from "./src/features/userManagement";
 import shoppingListManagentReducer from "./src/features/shoppingListManagement";
-
+import sharerShoppingListManagementReducer from "./src/features/sharerListmanagement";
 // ...
 
 export const store = configureStore({
   reducer: {
     userManagement: userManagementReducer,
     shoppingListManagent: shoppingListManagentReducer,
+    sharerListManagement : sharerShoppingListManagementReducer
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
