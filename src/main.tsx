1a3f21c3a8a7afbd06f636f7d103baf30db36924
siyabonga <siyabongakhanyile76@gmail.com>
2026-03-20 16:11:14 +0200

    fix:removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "../store.ts";
 import "sonner/dist/styles.css";
-import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- store.ts
+++ store.ts
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-import { persistReducer, persistStore } from "redux-persist";
+import { persistReducer} from "redux-persist";
 import userManagementReducer from "./src/features/userManagement";
 import shoppingListManagentReducer from "./src/features/shoppingListManagement";
 import sharerShoppingListManagementReducer from "./src/features/sharerListmanagement";
